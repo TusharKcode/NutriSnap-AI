95f86da68d5519ec1603eaf1ed831271a61d469a
Tushar Kumar <tkgoyal104@gmail.com>
2026-06-14 10:47:05 +0530

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "YOUR_FRONTEND_URL"
+            "process.env.FRONTEND_URL"
         ],
         credentials: true
     })
